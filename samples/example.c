c5a813ca526d3c3afc8b3d983f68d9efbe2f69e6
José Fonseca <jose.r.fonseca@gmail.com>
2013-06-19 22:46:24 +0100

    Resolve line in example.

--- samples/example.c
+++ samples/example.c
@@ -16,9 +16,9 @@ int main() {
     assert(bRet);
 
 #if 0
-    DWORD Address = (DWORD64)(UINT_PTR)GetProcAddress(LoadLibraryA("user32.dll"), "MessageBoxA");
+    DWORD64 dwAddr = (DWORD64)(UINT_PTR)GetProcAddress(LoadLibraryA("user32.dll"), "MessageBoxA");
 #else
-    DWORD Address = (DWORD64)(UINT_PTR)&foo;
+    DWORD64 dwAddr = (DWORD64)(UINT_PTR)&foo;
 #endif
 
     DWORD64 Displacement = 0;
@@ -29,10 +29,20 @@ int main() {
     memset(&s, 0, sizeof s);
     s.Symbol.SizeOfStruct = sizeof s.Symbol;
     s.Symbol.MaxNameLen = sizeof s.Symbol.Name + sizeof s.Name;
-    bRet = SymFromAddr(hProcess, Address, &Displacement, &s.Symbol);
+    bRet = SymFromAddr(hProcess, dwAddr, &Displacement, &s.Symbol);
     printf("bRet = %i\n", bRet);
     if (bRet) {
         printf("Symbol.Name = %s\n", s.Symbol.Name);
+    }
+
+    DWORD dwDisplacement;
+    IMAGEHLP_LINE64 Line;
+    ZeroMemory(&Line, sizeof Line);
+    Line.SizeOfStruct = sizeof Line;
+    bRet = SymGetLineFromAddr64(hProcess, dwAddr, &dwDisplacement, &Line);
+    if (bRet) {
+        printf("Line.LineNumber = %lu\n", Line.LineNumber);
+        printf("Line.FileName = %s\n", Line.FileName);
     }
 
     SymCleanup(hProcess);
