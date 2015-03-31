183ce274b17fab7bef960f8d5b0bca37728cb520
Jose Fonseca <jose.r.fonseca@gmail.com>
2015-03-31 12:07:58 +0100

    mgwhelp: move PEGetImageBase into mgwhelp.

--- src/mgwhelp/mgwhelp.c
+++ src/mgwhelp/mgwhelp.c
@@ -29,7 +29,6 @@
 
 #include "outdbg.h"
 #include "demangle.h"
-#include "pehelp.h"
 #include "mgwhelp.h"
 
 #include "dwarf_pe.h"
@@ -60,6 +59,66 @@ struct mgwhelp_process
 
 
 struct mgwhelp_process *processes = NULL;
+
+
+/* We must use a memory map of the file, not read memory directly, as the
+ * value of ImageBase in memory changes.
+ */
+static DWORD64
+PEGetImageBase(const char *szImageName)
+{
+    HANDLE hFile;
+    HANDLE hFileMapping;
+    PBYTE lpFileBase;
+    PIMAGE_DOS_HEADER pDosHeader;
+    PIMAGE_NT_HEADERS pNtHeaders;
+    PIMAGE_OPTIONAL_HEADER pOptionalHeader;
+    PIMAGE_OPTIONAL_HEADER32 pOptionalHeader32;
+    PIMAGE_OPTIONAL_HEADER64 pOptionalHeader64;
+    DWORD64 ImageBase = 0;
+
+    hFile = CreateFile(szImageName, GENERIC_READ, FILE_SHARE_READ, NULL,
+                       OPEN_EXISTING, FILE_ATTRIBUTE_NORMAL, 0);
+    if (hFile == INVALID_HANDLE_VALUE) {
+        goto no_file;
+    }
+
+    hFileMapping = CreateFileMapping(hFile, NULL, PAGE_READONLY, 0, 0, NULL);
+    if (!hFileMapping) {
+        goto no_file_mapping;
+    }
+
+    lpFileBase = (PBYTE)MapViewOfFile(hFileMapping, FILE_MAP_READ, 0, 0, 0);
+    if (!lpFileBase) {
+        goto no_view_of_file;
+    }
+
+    pDosHeader = (PIMAGE_DOS_HEADER)lpFileBase;
+
+    pNtHeaders = (PIMAGE_NT_HEADERS)(lpFileBase + pDosHeader->e_lfanew);
+
+    pOptionalHeader = &pNtHeaders->OptionalHeader;
+    pOptionalHeader32 = (PIMAGE_OPTIONAL_HEADER32)pOptionalHeader;
+    pOptionalHeader64 = (PIMAGE_OPTIONAL_HEADER64)pOptionalHeader;
+
+    switch (pOptionalHeader->Magic) {
+    case IMAGE_NT_OPTIONAL_HDR32_MAGIC :
+        ImageBase = pOptionalHeader32->ImageBase;
+        break;
+    case IMAGE_NT_OPTIONAL_HDR64_MAGIC :
+        ImageBase = pOptionalHeader64->ImageBase;
+        break;
+    default:
+        assert(0);
+    }
+
+no_view_of_file:
+    CloseHandle(hFileMapping);
+no_file_mapping:
+    CloseHandle(hFile);
+no_file:
+    return ImageBase;
+}
 
 
 static struct mgwhelp_module *
