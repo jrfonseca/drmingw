jrfonseca/drmingw

commit 9de22fe807c1bfb1a4cc98c9e26d53527f9d23ae

mgwhelp: Fix corruption of module linked list.

src/mgwhelp/mgwhelp.c
@@ -139,9 +139,6 @@ mgwhelp_module_create(struct mgwhelp_process * process, DWORD64 Base)
 
     module->Base = Base;
 
-    module->next = process->modules;
-    process->modules = module;
-
     /* SymGetModuleInfo64 is not reliable for this, as explained in
      * https://msdn.microsoft.com/en-us/library/windows/desktop/ms681336.aspx
      */
@@ -150,6 +147,7 @@ mgwhelp_module_create(struct mgwhelp_process * process, DWORD64 Base)
                                  module->LoadedImageName,
                                  sizeof module->LoadedImageName);
     if (dwRet == 0) {
+        OutputDebug("MGWHELP: could not determined module name\n");
         goto no_module_name;
     }
 
@@ -162,10 +160,12 @@ mgwhelp_module_create(struct mgwhelp_process * process, DWORD64 Base)
         OutputDebug("MGWHELP: %s: %s\n", module->LoadedImageName, "no dwarf symbols");
     }
 
+    module->next = process->modules;
+    process->modules = module;
+
     return module;
 
 no_module_name:
-    OutputDebug("MGWHELP: no module name");
     free(module);
 no_module:
     return NULL;

commit 69de8104c1265ff0501f25e0ab478686cadb2837

mgwhelp: Don't leak modules.
Regression introduced with 9de22fe807c1bfb1a4cc98c9e26d53527f9d23ae

src/mgwhelp/mgwhelp.c
@@ -154,9 +154,7 @@ mgwhelp_module_create(struct mgwhelp_process * process, DWORD64 Base)
     module->image_base_vma = PEGetImageBase(module->LoadedImageName);
 
     Dwarf_Error error = 0;
-    if (dwarf_pe_init(module->LoadedImageName, 0, 0, &module->dbg, &error) == DW_DLV_OK) {
-        return module;
-    } else {
+    if (dwarf_pe_init(module->LoadedImageName, 0, 0, &module->dbg, &error) != DW_DLV_OK) {
         OutputDebug("MGWHELP: %s: %s\n", module->LoadedImageName, "no dwarf symbols");
     }
 
@@ -214,16 +212,7 @@ mgwhelp_process_lookup(HANDLE hProcess)
         process = process->next;
     }
 
-    process = (struct mgwhelp_process *)calloc(1, sizeof *process);
-    if (!process)
-        return process;
-
-    process->hProcess = hProcess;
-
-    process->next = processes;
-    processes = process;
-
-    return process;
+    return NULL;
 }
 
 