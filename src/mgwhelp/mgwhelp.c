9f62d9587c6606a1a230fcf0d86ca4d1f01db4e4
Jose Fonseca <jose.r.fonseca@gmail.com>
2015-04-24 23:09:50 +0100

    mgwhelp: Implement SymLoadModuleExW too.

--- src/mgwhelp/mgwhelp.c
+++ src/mgwhelp/mgwhelp.c
@@ -362,6 +362,38 @@ MgwSymLoadModuleEx(HANDLE hProcess,
 }
 
 
+DWORD64 WINAPI
+MgwSymLoadModuleExW(HANDLE hProcess,
+                    HANDLE hFile,
+                    PCWSTR ImageName,
+                    PCWSTR ModuleName,
+                    DWORD64 BaseOfDll,
+                    DWORD DllSize,
+                    PMODLOAD_DATA Data,
+                    DWORD Flags)
+{
+    DWORD dwRet;
+
+    dwRet = SymLoadModuleExW(hProcess, hFile, ImageName, ModuleName, BaseOfDll, DllSize, Data, Flags);
+
+    if (BaseOfDll) {
+        char ImageNameBuf[MAX_PATH];
+        PCSTR ImageNameA;
+
+        if (ImageName) {
+            WideCharToMultiByte(CP_ACP, 0, ImageName, -1, ImageNameBuf, _countof(ImageNameBuf), NULL, NULL);
+            ImageNameA = ImageNameBuf;
+        } else {
+            ImageNameA = NULL;
+        }
+
+        mgwhelp_module_lookup(hProcess, hFile, ImageNameA, BaseOfDll);
+    }
+
+    return dwRet;
+}
+
+
 /*
  * The GetModuleBase64 function retrieves the base address of the module that
  * contains the specified address.
