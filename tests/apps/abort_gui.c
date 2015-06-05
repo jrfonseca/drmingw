54336c676be960f041f3fedfce4f4b2b9ecbd20e
Jose Fonseca <jose.r.fonseca@gmail.com>
2015-06-05 12:53:04 +0100

    tests/apps: Try to force abort/assert_gui to popup a message box.

--- tests/apps/abort_gui.c
+++ tests/apps/abort_gui.c
@@ -25,11 +25,21 @@
  *
  **************************************************************************/
 
+#include <stdlib.h>
+#include <assert.h>
 #include <windows.h>
 
 int CALLBACK
 WinMain(HINSTANCE hInstance, HINSTANCE hPrevInstance, LPSTR lpCmdLine, int nCmdShow)
 {
+    // Enable assertion failure message box
+    // http://msdn.microsoft.com/en-us/library/sas1dkb2.aspx
+    _set_error_mode(_OUT_TO_MSGBOX);
+#ifdef _MSC_VER
+    // Enable abort message box
+    // http://msdn.microsoft.com/en-us/library/e631wekh.aspx
+    _set_abort_behavior(0, _CALL_REPORTFAULT);
+#endif
     abort();
 }
 
--- tests/apps/assert_gui.c
+++ tests/apps/assert_gui.c
@@ -27,12 +27,21 @@
 
 #undef NDEBUG
 
+#include <stdlib.h>
 #include <assert.h>
 #include <windows.h>
 
 int CALLBACK
 WinMain(HINSTANCE hInstance, HINSTANCE hPrevInstance, LPSTR lpCmdLine, int nCmdShow)
 {
+    // Enable assertion failure message box
+    // http://msdn.microsoft.com/en-us/library/sas1dkb2.aspx
+    _set_error_mode(_OUT_TO_MSGBOX);
+#ifdef _MSC_VER
+    // Enable abort message box
+    // http://msdn.microsoft.com/en-us/library/e631wekh.aspx
+    _set_abort_behavior(0, _CALL_REPORTFAULT);
+#endif
     assert(2 + 2 == 5);
     return 0;
 }
