cb8d703651f6696e2ffb37de7e0e129f1ff20785
Jose Fonseca <jose.r.fonseca@gmail.com>
2021-03-21 09:14:51 +0000

    libdwarf: Re-import as a git submodule.

--- thirdparty/libdwarf/libdwarf/libdwarf_version.h
+++
@@ -1,7 +0,0 @@
-/*
-    This date string is hereby put into the public domain.
-    Copyrighting this is crazy. It's just a date string
-    and is modified from time to time.
-*/
-
-#define DW_VERSION_DATE_STR " 2020-05-19 15:05:18-07:00  "
