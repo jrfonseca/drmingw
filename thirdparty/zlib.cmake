72836b0f66f499f42b88cfec715d9e125b59b23c
Jose Fonseca <jose.r.fonseca@gmail.com>
2021-03-21 08:20:59 +0000

    cmake: Move zlib CMake file out of tree.

---
+++ thirdparty/zlib.cmake
@@ -0,0 +1,32 @@
+add_library (z EXCLUDE_FROM_ALL
+    zlib/adler32.c
+    zlib/compress.c
+    zlib/crc32.c
+    zlib/deflate.c
+    zlib/gzclose.c
+    zlib/gzlib.c
+    zlib/gzread.c
+    zlib/gzwrite.c
+    zlib/inflate.c
+    zlib/infback.c
+    zlib/inftrees.c
+    zlib/inffast.c
+    zlib/trees.c
+    zlib/uncompr.c
+    zlib/zutil.c
+)
+
+# adjust warnings
+if (MSVC)
+    target_compile_options (z PRIVATE -wd4131) # uses old-style declarator
+else ()
+    target_compile_definitions (z PRIVATE HAVE_UNISTD_H)
+endif ()
+
+target_include_directories (z PUBLIC zlib)
+
+install (
+    FILES zlib/README
+    DESTINATION doc
+    RENAME LICENSE-zlib.txt
+)
