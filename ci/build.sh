91e943bac597de92a5e8a4ca92b44a64bbd7e01d
Jose Fonseca <jose.r.fonseca@gmail.com>
2021-04-04 14:10:13 +0100

    ci: Allow to control the build directory.

--- ci/build.sh
+++ ci/build.sh
@@ -2,16 +2,16 @@
 
 set -eux
 
-build () {
+cmake_ () {
 	test "$1" = "-B"
 	test -f "$2/CMakeCache.txt" || cmake "$@"
 }
 
-build_mingw32() {
-	build "$@" -DCMAKE_TOOLCHAIN_FILE=$PWD/.github/scripts/mingw32.cmake
+cmake_mingw32() {
+	cmake_ "$@" -DCMAKE_TOOLCHAIN_FILE=$PWD/.github/scripts/mingw32.cmake
 }
-build_mingw64() {
-	build "$@" -DCMAKE_TOOLCHAIN_FILE=$PWD/.github/scripts/mingw64.cmake
+cmake_mingw64() {
+	cmake_ "$@" -DCMAKE_TOOLCHAIN_FILE=$PWD/.github/scripts/mingw64.cmake
 }
 
 xvfb_run() {
@@ -27,9 +27,11 @@ cmake --version
 python3 --version
 $WINE --version
 
-mkdir -p build
+BUILD_DIR=${BUILD_DIR:-$PWD/build}
 
-export WINEPREFIX=$PWD/build/wine
+mkdir -p $BUILD_DIR
+
+export WINEPREFIX=$BUILD_DIR/wine
 
 # Prevent Gecko/Mono installation dialogues
 # https://forum.winehq.org/viewtopic.php?f=2&t=16320#p78458
@@ -37,21 +39,21 @@ export WINEDLLOVERRIDES="mscoree,mshtml="
 
 test -d $WINEPREFIX || xvfb_run $WINE wineboot.exe --init
 
-build_mingw64 -B build/mingw64 -S . -G Ninja -DCMAKE_BUILD_TYPE=${CMAKE_BUILD_TYPE:-Debug} -DWINE_PROGRAM=$WINE
-build_mingw32 -B build/mingw32 -S . -G Ninja -DCMAKE_BUILD_TYPE=${CMAKE_BUILD_TYPE:-Debug} -DWINE_PROGRAM=$WINE
-cmake --build build/mingw64 --target all
-cmake --build build/mingw32 --target all
-xvfb_run cmake --build build/mingw64 --target check
-xvfb_run cmake --build build/mingw32 --target check
+cmake_mingw64 -B $BUILD_DIR/mingw64 -S . -G Ninja -DCMAKE_BUILD_TYPE=${CMAKE_BUILD_TYPE:-Debug} -DWINE_PROGRAM=$WINE
+cmake_mingw32 -B $BUILD_DIR/mingw32 -S . -G Ninja -DCMAKE_BUILD_TYPE=${CMAKE_BUILD_TYPE:-Debug} -DWINE_PROGRAM=$WINE
+cmake --build $BUILD_DIR/mingw64 --target all
+cmake --build $BUILD_DIR/mingw32 --target all
+xvfb_run cmake --build $BUILD_DIR/mingw64 --target check
+xvfb_run cmake --build $BUILD_DIR/mingw32 --target check
 
-build_mingw64 -B build/apps/mingw64 -S tests/apps -G Ninja -DCMAKE_BUILD_TYPE=Debug
-cmake --build build/apps/mingw64 --target all
-build_mingw32 -B build/apps/mingw32 -S tests/apps -G Ninja -DCMAKE_BUILD_TYPE=Debug
-cmake --build build/apps/mingw32 --target all
+cmake_mingw64 -B $BUILD_DIR/apps/mingw64 -S tests/apps -G Ninja -DCMAKE_BUILD_TYPE=Debug
+cmake --build $BUILD_DIR/apps/mingw64 --target all
+cmake_mingw32 -B $BUILD_DIR/apps/mingw32 -S tests/apps -G Ninja -DCMAKE_BUILD_TYPE=Debug
+cmake --build $BUILD_DIR/apps/mingw32 --target all
 
 status=0
-xvfb_run python3 tests/apps/test.py -w $WINE "$@" build/mingw64/bin/catchsegv.exe build/apps/mingw64 build/apps/mingw32 || status=1
-xvfb_run python3 tests/apps/test.py -w $WINE "$@" build/mingw32/bin/catchsegv.exe build/apps/mingw32 || status=1
+xvfb_run python3 tests/apps/test.py -w $WINE "$@" $BUILD_DIR/mingw64/bin/catchsegv.exe $BUILD_DIR/apps/mingw64 $BUILD_DIR/apps/mingw32 || status=1
+xvfb_run python3 tests/apps/test.py -w $WINE "$@" $BUILD_DIR/mingw32/bin/catchsegv.exe $BUILD_DIR/apps/mingw32 || status=1
 
 if [ "$status" -ne "0" ]
 then
