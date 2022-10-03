3450a15f8a5e967f5061ad80433be84c38ace9f2
Jose Fonseca <jose.r.fonseca@gmail.com>
2022-10-03 14:19:56 +0100

    cmake: Allow using Posix threads.
    Unavoidable with newer MinGW versions.

--- cmake/StaticCRT.cmake
+++ cmake/StaticCRT.cmake
@@ -8,14 +8,25 @@ if (MINGW)
         set (CMAKE_SHARED_LINKER_FLAGS "${CMAKE_SHARED_LINKER_FLAGS} -static-libgcc")
         set (CMAKE_MODULE_LINKER_FLAGS "${CMAKE_MODULE_LINKER_FLAGS} -static-libgcc")
     endif ()
-    if (CMAKE_CXX_COMPILER_ID STREQUAL "Clang")
-        set (CMAKE_EXE_LINKER_FLAGS "${CMAKE_EXE_LINKER_FLAGS} -static")
-    else ()
+    if (CMAKE_CXX_COMPILER_ID STREQUAL "GNU")
         check_cxx_compiler_flag (-static-libstdc++ HAVE_STATIC_LIBSTDCXX_FLAG)
         if (HAVE_STATIC_LIBSTDCXX_FLAG)
             set (CMAKE_EXE_LINKER_FLAGS "${CMAKE_EXE_LINKER_FLAGS} -static-libstdc++")
             set (CMAKE_SHARED_LINKER_FLAGS "${CMAKE_SHARED_LINKER_FLAGS} -static-libstdc++")
             set (CMAKE_MODULE_LINKER_FLAGS "${CMAKE_MODULE_LINKER_FLAGS} -static-libstdc++")
+        endif ()
+
+        # Statically link Posix threads when detected.
+        execute_process (
+            COMMAND "${CMAKE_COMMAND}" -E echo "#include <thread>\n#ifdef _GLIBCXX_HAS_GTHREADS\n#error _GLIBCXX_HAS_GTHREADS\n#endif"
+            COMMAND "${CMAKE_CXX_COMPILER}" -x c++ -E -
+            RESULT_VARIABLE STATUS_CXX11_THREADS
+            OUTPUT_QUIET
+            ERROR_QUIET
+        )
+        if (NOT STATUS_CXX11_THREADS EQUAL 0)
+            # https://stackoverflow.com/a/28001271
+            set (CMAKE_CXX_STANDARD_LIBRARIES "-Wl,-Bstatic -lstdc++ -lpthread -Wl,-Bdynamic ${CMAKE_CXX_STANDARD_LIBRARIES}")
         endif ()
     endif ()
 endif ()
