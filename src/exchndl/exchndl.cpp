jrfonseca/drmingw

commit 1e07286ac7264ddf73066ba82416a63731045cd7

Correctly handle Unicode file names in ExcHndl
Add `ExcHndlSetLogFileNameW` that accepts a Unicode wide character string as an alternative to `ExcHndlSetLogFileNameA`, which uses the current Windows code page.
Internally only the wide character string is stored. The file name passed to `ExcHndlSetLogFileNameA` is converted to wide characters using the current code page.

Use `GetModuleFileNameW` instead of `GetModuleFileNameA` to handle the case that the module file name contains Unicode characters.

Don't use `%s` in format strings to print the argument when it's a nullptr, as this is undefined behavior.

src/exchndl/exchndl.cpp
@@ -37,7 +37,7 @@
 // Declare the static variables
 static BOOL g_bHandlerSet = FALSE;
 static LPTOP_LEVEL_EXCEPTION_FILTER g_prevExceptionFilter = nullptr;
-static std::string g_szLogFileName;
+static std::wstring g_szLogFileName = L"";
 static HANDLE g_hReportFile = nullptr;
 static BOOL g_bOwnReportFile = FALSE;
 
@@ -143,12 +143,12 @@ TopLevelExceptionFilter(PEXCEPTION_POINTERS pExceptionInfo)
             SetErrorMode(SEM_FAILCRITICALERRORS | SEM_NOGPFAULTERRORBOX | SEM_NOOPENFILEERRORBOX);
 
         if (!g_hReportFile) {
-            if (g_szLogFileName == "-") {
+            if (g_szLogFileName == L"-") {
                 g_hReportFile = GetStdHandle(STD_ERROR_HANDLE);
                 g_bOwnReportFile = FALSE;
-            } else {
+            } else if (!g_szLogFileName.empty()) {
                 g_hReportFile =
-                    CreateFileA(g_szLogFileName.c_str(), GENERIC_WRITE,
+                    CreateFileW(g_szLogFileName.c_str(), GENERIC_WRITE,
                                 FILE_SHARE_READ | FILE_SHARE_WRITE, 0, OPEN_ALWAYS, 0, 0);
                 g_bOwnReportFile = TRUE;
             }
@@ -180,9 +180,9 @@ Setup(void)
 
     // Figure out what the report file will be named, and store it away
     DWORD nSize = MAX_PATH;
-    std::vector<char> path(nSize);
+    std::vector<WCHAR> path(nSize);
     DWORD dwRet;
-    while ((dwRet = GetModuleFileNameA(nullptr, &path[0], nSize)) == nSize) {
+    while ((dwRet = GetModuleFileNameW(nullptr, &path[0], nSize)) == nSize) {
         nSize *= 2;
         path.resize(nSize);
     }
@@ -190,19 +190,19 @@ Setup(void)
         // Look for the '.' before the "EXE" extension.  Replace the extension
         // with "RPT"
         path.resize(nSize);
-        auto found = std::find(path.crbegin(), path.crend(), '.');
+        auto found = std::find(path.crbegin(), path.crend(), L'.');
         if (found != path.crend()) {
             // Reverse iterators here, hence crend() points to string start
             path.resize(path.crend() - found - 1);
         }
-        path.push_back('.');
-        path.push_back('R');
-        path.push_back('P');
-        path.push_back('T');
+        path.push_back(L'.');
+        path.push_back(L'R');
+        path.push_back(L'P');
+        path.push_back(L'T');
         g_szLogFileName.replace(g_szLogFileName.begin(), g_szLogFileName.end(), &path[0],
                                 path.size());
     } else {
-        g_szLogFileName = "EXCHNDL.RPT";
+        g_szLogFileName = L"EXCHNDL.RPT";
     }
 }
 
@@ -251,13 +251,28 @@ ExcHndlInit(void)
 
 
 BOOL APIENTRY
-ExcHndlSetLogFileNameA(const char *szLogFileName)
+ExcHndlSetLogFileNameA(const char *pszLogFileName)
 {
-    if (!szLogFileName) {
-        OutputDebug("EXCHNDL: specified log name is invalid (%s)\n", szLogFileName);
+    if (!pszLogFileName) {
+        OutputDebug("EXCHNDL: specified log name is invalid (nullptr)\n");
         return FALSE;
     }
-    g_szLogFileName = szLogFileName;
+    int nSize = MultiByteToWideChar(CP_ACP, 0, pszLogFileName, -1, nullptr, 0);
+    std::vector<WCHAR> wideLogFileName(nSize);
+    assert(MultiByteToWideChar(CP_ACP, 0, pszLogFileName, -1, &wideLogFileName[0], nSize) != 0);
+    g_szLogFileName = &wideLogFileName[0];
+    return TRUE;
+}
+
+
+BOOL APIENTRY
+ExcHndlSetLogFileNameW(const WCHAR *pszLogFileName)
+{
+    if (!pszLogFileName) {
+        OutputDebug("EXCHNDL: specified log name is invalid (nullptr)\n");
+        return FALSE;
+    }
+    g_szLogFileName = pszLogFileName;
     return TRUE;
 }
 

include/exchndl.h
@@ -31,9 +31,22 @@ ExcHndlInit(void);
 
 
 // Override the report file name.
+// The file name must be encoded for the current Windows code page.
+// The default code page does not support Unicode characters,
+// unless you are targeting Windows Version 1903 or later.
 //
 // Default is prog_name.RPT, in the same directory as the main executable.
 //
 // You can also pass "-" for stderr.
 EXTERN_C BOOL APIENTRY
-ExcHndlSetLogFileNameA(const char *szLogFileName);
+ExcHndlSetLogFileNameA(const char *pszLogFileName);
+
+
+// Override the report file name.
+// The file name is encoded as Unicode wide characters.
+//
+// Default is prog_name.RPT, in the same directory as the main executable.
+//
+// You can also pass L"-" for stderr.
+EXTERN_C BOOL APIENTRY
+ExcHndlSetLogFileNameW(const WCHAR *pszLogFileName);

src/exchndl/exchndl32exp.def
@@ -3,3 +3,4 @@ LIBRARY exchndl.dll
 EXPORTS
     ExcHndlInit = ExcHndlInit@0
     ExcHndlSetLogFileNameA = ExcHndlSetLogFileNameA@4
+    ExcHndlSetLogFileNameW = ExcHndlSetLogFileNameW@4

src/exchndl/exchndl32imp.def
@@ -3,3 +3,4 @@ LIBRARY exchndl.dll
 EXPORTS
     ExcHndlInit@0
     ExcHndlSetLogFileNameA@4
+    ExcHndlSetLogFileNameW@4

src/exchndl/exchndl64.def
@@ -3,3 +3,4 @@ LIBRARY exchndl.dll
 EXPORTS
     ExcHndlInit
     ExcHndlSetLogFileNameA
+    ExcHndlSetLogFileNameW

commit 8896a3ecee7123d8bf2da224f8f47968ffb20b4b

exchndl: Fix ExcHndlSetLogFileNameA on release builds.
Fixes: 1e07286ac7264ddf73066ba82416a63731045cd7

src/exchndl/exchndl.cpp
@@ -270,7 +270,7 @@ ExcHndlSetLogFileNameA(const char *pszLogFileName)
     }
     int nSize = MultiByteToWideChar(CP_ACP, 0, pszLogFileName, -1, nullptr, 0);
     std::vector<WCHAR> wideLogFileName(nSize);
-    assert(MultiByteToWideChar(CP_ACP, 0, pszLogFileName, -1, &wideLogFileName[0], nSize) != 0);
+    MultiByteToWideChar(CP_ACP, 0, pszLogFileName, -1, &wideLogFileName[0], nSize);
     g_szLogFileName = &wideLogFileName[0];
     return TRUE;
 }