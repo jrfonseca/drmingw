f9c837ede6987b4c14798606c4c8e6a7e24a3f9d
Jose Fonseca <jose.r.fonseca@gmail.com>
2023-12-09 13:16:24 +0000

    ci: Temporary workaround for build race condition.

--- ci/build.ps1
+++ ci/build.ps1
@@ -119,6 +119,10 @@ Exec { cmake "-S." "-B$buildDir" -G $generator "-DCMAKE_BUILD_TYPE=$CMAKE_BUILD_
 #
 # Build
 #
+if ($target -eq "mingw32") {
+# XXX: Temporary hack to avoid "No rule to make target lib*.dll.a"
+    Exec { cmake --build $buildDir --use-stderr --target libmgwhelp_implib --target libexchndl_implib }
+}
 Exec { cmake --build $buildDir --use-stderr --target all }
 
 #
