4f117cbe7b13026bc05a4f11c53642cc268adc8d
Jose Fonseca <jose.r.fonseca@gmail.com>
2026-04-27 22:12:41 +0100

    github: Cache docker image.

--- ci/build-docker.sh
+++ ci/build-docker.sh
@@ -24,11 +24,21 @@ docker_run () {
 
 if ! docker buildx version
 then
-   echo 'error: docker buildx plugin required (sudo apt install docker-buildx)'
+   echo 'error: docker buildx plugin required (sudo apt install docker-buildx)' 1>&2
    exit 1
 fi
 
-docker buildx build -t $docker_tag -f ci/docker/$docker_file.Dockerfile ci/docker
+if [ "$GITHUB_ACTIONS" = true ]
+then
+    # See:
+    # - https://dev.to/dtinth/caching-docker-builds-in-github-actions-which-approach-is-the-fastest-a-research-18ei
+    # - https://github.com/dtinth/github-actions-docker-layer-caching-poc/blob/master/.github/workflows/dockerimage.yml
+    docker pull docker.pkg.github.com/$GITHUB_REPOSITORY/build-cache || true
+    docker buildx build -t $docker_tag --cache-from=docker.pkg.github.com/$GITHUB_REPOSITORY/build-cache -f ci/docker/$docker_file.Dockerfile ci/docker
+    docker tag $docker_tag docker.pkg.github.com/$GITHUB_REPOSITORY/build-cache && docker push docker.pkg.github.com/$GITHUB_REPOSITORY/build-cache || true
+else
+    docker buildx build -t $docker_tag -f ci/docker/$docker_file.Dockerfile ci/docker
+fi
 
 # Set `build_script=ci/build-clang.sh` to build and run Clang tests.
 docker_run ${build_script:-ci/build.sh} "$@"
